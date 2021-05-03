59fa39ba01133fcd297d76948dba68ad8cdc205d
THE-TPS <75592613+THE-TPS@users.noreply.github.com>
2021-05-03 12:16:30 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+basic.showIcon(IconNames.Heart)
+basic.clearScreen()
+basic.pause(10)
+basic.showIcon(IconNames.Angry)
+basic.clearScreen()
+basic.pause(10)
+basic.showIcon(IconNames.Heart)
+basic.clearScreen()
+basic.pause(10)
+basic.forever(function on_forever() {
+    
+})
